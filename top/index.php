9078b7cd96cd0f3edb1c881d089a7833a1f9ea02
odomiwith <mimimimatcha@gmail.com>
2018-02-14 22:24:30 +0900

    トップ画面の作成

---
+++ top/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="utf-8">
+<meta  name="viewport" content="width=device-width,initial-scale=1">
+<meta name="robots" content="noindex,nofollow">
+<!-- アイコンの設定 -->
+<!-- <link rel="icon" href="#" sizes="16x16" type="images/png >"-->
+<!-- <link rel="apple-touch-icon-precomposed" href="#" />"-->
+<link rel="stylesheet" href="index.css">
+<script src="index.js"></script>
+<title>うぇるかめ</title>
+</head>
+
+<body>
+
+<a href="../info/index.php?name=r">あそた</a>
+<a href="../info/index.php?name=m">うみゆ</a>
+
+</body>
+</html>
+
+
