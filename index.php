5f2663f4bc2045d2c3fb62e3933d99ae6639cc54
odomiwith <mimimimatcha@gmail.com>
2018-02-17 22:01:37 +0900

    セッション持たせて各コンテンツの構造作ったお

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
-<?php
-for($i=0; $i<99; $i++)
-{
-  print $i;
-}
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta  name="viewport" content="width=device-width,initial-scale=1">
+<meta name="robots" content="noindex,nofollow">
+<!-- アイコンの設定 -->
+<!-- <link rel="icon" href="#" sizes="16x16" type="images/png >"-->
+<!-- <link rel="apple-touch-icon-precomposed" href="#" />"-->
+<link rel="stylesheet" href="index.css">
+<script src="index.js"></script>
+<title>うぇるかめ</title>
+</head>
+<body>
+  <form method="post" name="login_ryo" action="info/">
+    <input type="hidden" name="user" value="ryo">
+    <a href="javascript:login_ryo.submit()">あそた</a>
+  </form>
+  <form method="post" name="login_meyou" action="info/">
+    <input type="hidden" name="user" value="meyou">
+    <a href="javascript:login_meyou.submit()">うみゆ</a>
+  </form>
+</body>
+</html>
+
--- info/index.php
+++ info/index.php
@@ -1,9 +1,37 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta  name="viewport" content="width=device-width,initial-scale=1">
+<meta name="robots" content="noindex,nofollow">
+<!-- アイコンの設定 -->
+<!-- <link rel="icon" href="#" sizes="16x16" type="images/png >"-->
+<!-- <link rel="apple-touch-icon-precomposed" href="#" />"-->
+<link rel="stylesheet" href="index.css">
+<script src="index.js"></script>
+<title>うぇるかめ</title>
+</head>
+<body>
 
-if($_GET["name"]=="r"){
-    print "おそた";
-}
-if($_GET["name"]=="m"){
-    print "みゆちゃん可愛い";
-}
+<?php
+    session_start();
+
+    if ($_SESSION['user'] == 'ryo' || $_POST['user'] == 'ryo') {
+        $_SESSION['user'] = 'ryo';
+        print "あざとい、いい加減やめたら？";
+    } elseif ($_SESSION['user'] == 'meyou' || $_POST['user'] == 'meyou') {
+        $_SESSION['user'] = 'meyou';
+        print "舞茸";
+    } else {
+        print "error";
+    }
+?>
+
+  <a href="../contents/zangyo">残業申請</a>
+  <a href="../contents/share">ナレッジシェア</a>
+  <a href="../contents/chat">チャット</a>
+</body>
+</html>
+
+
 
