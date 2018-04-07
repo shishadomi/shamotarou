4e5f02de6afed22cf33f156b200b6352afaa1268
osotawith <u_n_d_e_r_b_a_r_@ezweb.ne.jp>
2018-04-07 12:20:34 +0900

    今日の勤怠をTOPへ表示

--- index.php
+++ index.php
@@ -20,5 +20,17 @@
     <input type="hidden" name="user" value="meyou">
     <a href="javascript:login_meyou.submit()">うみゆ</a>
   </form>
+ <?php
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=shisha;charset=utf8', 'root', '', $except);
+    $smt = $pdo->query('select * from chat_room where room_flg = 1 order by id desc limit 1');
+	// 実行結果を配列に返す。
+	$select_data = $smt->fetchAll();
+} catch (PDOException $e) {
+    exit('データベース接続失敗。'.$e->getMessage());
+}
+
+var_dump($select_data)
+ ?>
 </body>
 </html>
