3a6d5a95eb13b3b63b49e7312e933dd2d4efd4d8
odomiwith <mimimimatcha@gmail.com>
2018-05-05 20:33:53 +0900

    とりあえずシェアのプシュー

---
+++ contents/share/ex/index.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta  name="viewport" content="width=device-width,initial-scale=1">
+<meta name="robots" content="noindex,nofollow">
+<!-- アイコンの設定 -->
+<!-- <link rel="icon" href="#" sizes="16x16" type="images/png >"-->
+<!-- <link rel="apple-touch-icon-precomposed" href="#" />"-->
+<link rel="stylesheet" href="index.css">
+<script src="index.js"></script>
+<title>いずくまブログ</title>
+</head>
+<body>
+  <div class="contentsArea">
+    <div class="backArea">
+      <div class="card">
+        <img src="images/mame.jpg" alt="Avatar" style="width:100%">
+          <div class="container">
+            <h4><b>title</b></h4> 
+            <p>なんか入ります</p> 
+          </div>
+      </div>
+    </div>
+    <div class="frontArea">frontend/design</div>
+    <div class="extraArea">extra</div>
+  </div>
+
+</body>
+</html>
