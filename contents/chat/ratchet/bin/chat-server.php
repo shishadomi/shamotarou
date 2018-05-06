717cb25ddb4eac70ec0bef5633dc2ea338effeef
osotawith <u_n_d_e_r_b_a_r_@ezweb.ne.jp>
2018-05-06 11:04:27 +0900

    websocketサーバ側からメッセージ登録

--- contents/chat/ratchet/bin/chat-server.php
+++ contents/chat/ratchet/bin/chat-server.php
@@ -3,13 +3,9 @@ use Ratchet\Server\IoServer;
 use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
 use MyApp\Chat;
- ///Users/sodaryoutarou/Desktop/develop/mi_ryo_system/www/contents/chat/ratchet/vendor/autoload.php
-    
-    // var_dump(__DIR__);
 
     require_once '/var/www/html/contents/chat/ratchet/vendor/autoload.php';
     
- 
     $server = IoServer::factory(
         new HttpServer(
             new WsServer(
